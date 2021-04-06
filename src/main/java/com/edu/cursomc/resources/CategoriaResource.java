058fc51422a6048a70abed325fdabbd4bdbf9d65
Eduardo Buchele <eduardobuchele@gmail.com>
2021-04-06 11:02:01 -0300

    Adicionado banco H2

--- src/main/java/com/edu/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edu/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.edu.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		List<Categoria> lCategoria = new ArrayList<>();
+		lCategoria.add(c1);
+		lCategoria.add(c2);
+		
+		return lCategoria;
 	}
 }
