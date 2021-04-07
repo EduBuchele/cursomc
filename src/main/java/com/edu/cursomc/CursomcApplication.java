2c8981c26913555f7a84f3ec9963b7dba2639d16
Eduardo Buchele <eduardobuchele@gmail.com>
2021-04-07 09:02:01 -0300

    Criado operacao de instanciacao

--- src/main/java/com/edu/cursomc/CursomcApplication.java
+++ src/main/java/com/edu/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.edu.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.edu.cursomc.domain.Categoria;
+import com.edu.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaDAO;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
