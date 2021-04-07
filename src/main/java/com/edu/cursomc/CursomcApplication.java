dba3c5f13ddb82718c2a6ea8ef2d7fd394aab72a
Eduardo Buchele <eduardobuchele@gmail.com>
2021-04-07 09:33:02 -0300

    Adicionado produtos

--- src/main/java/com/edu/cursomc/CursomcApplication.java
+++ src/main/java/com/edu/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.edu.cursomc.domain.Categoria;
+import com.edu.cursomc.domain.Produto;
 import com.edu.cursomc.repositories.CategoriaRepository;
+import com.edu.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaDAO;
+	@Autowired
+	private ProdutoRepository produtoDAO;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +29,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00 );
+		Produto p2 = new Produto(null, "Impressora", 800.00 );
+		Produto p3 = new Produto(null, "Mouse", 80.00 );
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
+		produtoDAO.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 	}
 
