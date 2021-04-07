b61bb90768e79e185d50b0766353b132eaa75777
Eduardo Buchele <eduardobuchele@gmail.com>
2021-04-07 16:23:20 -0300

    Atualizacao JsonIgnore

--- src/main/java/com/edu/cursomc/domain/Produto.java
+++ src/main/java/com/edu/cursomc/domain/Produto.java
@@ -3,7 +3,6 @@ package com.edu.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -16,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -29,7 +27,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "produto_id"),
