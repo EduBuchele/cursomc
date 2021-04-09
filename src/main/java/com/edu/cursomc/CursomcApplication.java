54f3ecb2574a0c3579bcf6a7144b92c73b3fc93e
Eduardo Buchele <eduardobuchele@gmail.com>
2021-04-09 08:52:39 -0300

    Add Paginacao com params opcionais

--- src/main/java/com/edu/cursomc/CursomcApplication.java
+++ src/main/java/com/edu/cursomc/CursomcApplication.java
@@ -64,6 +64,11 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Categoria03");
+		Categoria cat4 = new Categoria(null, "Categoria04");
+		Categoria cat5 = new Categoria(null, "Categoria05");
+		Categoria cat6 = new Categoria(null, "Categoria06");
+		Categoria cat7 = new Categoria(null, "Categoria07");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00 );
 		Produto p2 = new Produto(null, "Impressora", 800.00 );
@@ -76,7 +81,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
+		categoriaDAO.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoDAO.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/edu/cursomc/services/CategoriaService.java
+++ src/main/java/com/edu/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edu.cursomc.domain.Categoria;
@@ -25,18 +28,15 @@ public class CategoriaService {
 								+ ", Tipo: " + Categoria.class.getName()));
 	}
 
-	
 	public Categoria insert (Categoria obj) {
 		obj.setId(null);
 		return dao.save(obj);
 	}
 
-
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return dao.save(obj);
 	}
-
 
 	public void delete(Integer id) {
 		try {
@@ -47,8 +47,12 @@ public class CategoriaService {
 		
 	}
 
-
 	public List<Categoria> findAll() {
 		return dao.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return dao.findAll(pageRequest);
 	}
 }
